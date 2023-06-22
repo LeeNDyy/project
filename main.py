9d00a4cdd922d92735b8007e26d75249e4f3795f
Arsenij <vasilkovarsenij3@gmail.com>
2023-06-22 22:47:03 +0300

    Данная программа проверяет, является ли введеное слово палиндромом, использую функцию is_palindrom(). Функция принимает на вход строку и возвращает True, если строка является палиндромом, если строка не является палиндромом, то выводит False. Чтобы проверить равна ли это страка обратной ей строке я использова срез [::-1].

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+def is_palindrome(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+input_string = input("Введите строку: ")
+if is_palindrome(input_string):
+    print("True")
+else:
+    print("False")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
